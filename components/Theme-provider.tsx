acabe62740ffd446d1d16c665669901a5bfe3c05
Developer Vikki <developervikki@zohomail.in>
2026-07-17 08:48:34 -0700

    Comment out Footer component in Theme-provider
    Comment out the Footer component in Theme-provider.

--- components/Theme-provider.tsx
+++ components/Theme-provider.tsx
@@ -13,7 +13,7 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
       disableTransitionOnChange
     >
       {children}
-      <Footer />
+      {/* <Footer /> */}
     </NextThemesProvider>
   );
 }
